899e04cc156c1c425b3214f58e0227e8c89c9e2c
SkyYong <yongzhu2014@outlook.com>
2019-09-12 23:34:24 +0800

    CAS

---
+++ 04_Thread/src/main/java/com/example/skylaker/ThreadLocalService.java
@@ -0,0 +1,39 @@
+package com.example.skylaker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 线程本地变量 ThreadLocal
+ * @author skylaker2019@163.com
+ * @version V1.0 2019/8/22 10:27 PM
+ */
+public class ThreadLocalService {
+    private static ThreadLocal<SimpleDateFormat> threadLocal =
+            new ThreadLocal<>();
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newFixedThreadPool(20);
+        for(;;){
+            pool.execute(new MyTask());
+        }
+    }
+
+
+    static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            if(null == threadLocal.get()){
+                threadLocal.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            }
+
+            String date = threadLocal.get().format(new Date(Math.abs(new Random().nextLong())));
+            System.out.println(date);
+        }
+    }
+
+}
